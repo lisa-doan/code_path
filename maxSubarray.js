e758846e467100380c1285ba895ff742ffbdeb9d
Lisa Doan <ldoan24@gmail.com>
2021-07-27 20:38:13 +0000

    added maxSubarray

---
+++ maxSubarray.js
@@ -0,0 +1,21 @@
+//find the maximum sun of a contingous subarray of 3
+
+//method: sliding window
+
+function maxSubarray(arr, k){
+  let maxValue = -Infinity;
+  let currentRunningSum = 0;
+  for(let i = 0; i < arr.length; i++) {
+    currentRunningSum += arr[i];
+    if(i >= k - 1) {
+      maxValue = Math.max(currentRunningSum, maxValue);
+      currentRunningSum -= arr[i - (k - 1)];
+    }
+  }
+return maxValue;
+}
+
+let array1 = [4, 2, 1, 7, 8, 1, 2, 8, 1, 0]
+console.log(maxSubarray(array1, 3))
+
+
--- reverseString.js
+++ reverseString.js
@@ -10,4 +10,3 @@ function reverseString(string){
 }
 
 console.log(reverseString('dog'))
-
