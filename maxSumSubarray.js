9cf47372e5fd6aa86aa733c26ac8ffee1e620f58
Lisa Doan <ldoan24@gmail.com>
2021-07-27 20:42:40 +0000

    updated function name

--- maxSumSubarray.js
+++ maxSumSubarray.js
@@ -2,7 +2,7 @@
 
 //method: sliding window
 
-function maxSubarray(arr, k){
+function maxSumSubarray(arr, k){
   let maxValue = -Infinity;
   let currentRunningSum = 0;
   for(let i = 0; i < arr.length; i++) {
@@ -16,6 +16,6 @@ return maxValue;
 }
 
 let array1 = [4, 2, 1, 7, 8, 1, 2, 8, 1, 0]
-console.log(maxSubarray(array1, 3))
+console.log(maxSumSubarray(array1, 3))
 
 
